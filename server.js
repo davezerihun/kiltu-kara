4f1a31376b25d47596a98a76a5d41c3047f73c36
unknown <dawitzarihun562@gmail.com>
2026-08-22 01:39:07 -0700

    Increase body parser payload limit and compress uploaded photo

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '15mb' })); 
-app.use(express.urlencoded({ extended: true, limit: '15mb' }));
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
